7728feaf011265be921653751d105b791ebf7220
johngalarza1970 <johngalarza1970@gmail.com>
2025-08-09 18:30:03 -0500

    feat: Service Worker actualizado con lo ultimo hecho en clase

--- sw.js
+++ sw.js
@@ -1,10 +1,33 @@
-self.addEventListener('install', (event) => {
-    console.info('Service Worker installing...');
+self.addEventListener("install", (event) => {
+  console.log("Almacenando archivos en cache");
+  const WU = new Promise((resolve, reject) => {
+    try {
+      setTimeout(() => {
+        console.log("Service Worker installed");
+        resolve();
+      }, 1000);
+      self.skipWaiting();
+    } catch (error) {
+      reject(error);
+    }
+  });
+  event.waitUntil(WU);
 });
 
-self.addEventListener('activate', (event) => {
-    console.info('Service Worker activating...');
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activado");
+  event.waitUntil(clients.claim());
 });
-self.addEventListener('fetch', (event) => {
-    console.info('Service Worker fetching:');
+
+self.addEventListener("fetch", (event) => {
+  console.log("Service Worker recibiendo una solicitud");
+  console.log(event.request.url);
 });
+
+self.addEventListener("sync", (event)=>{
+    console.log(event)
+});
+
+self.addEventListener("push", (event)=>{
+    console.log("Push notificacion recevied", event)
+})
